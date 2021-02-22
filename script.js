43967e47e92c60b3cc221da07208dd68c6eb363b
nath4ns <nathansousa360@gmail.com>
2021-02-21 22:20:29 -0300

    avançando

---
+++ script.js
@@ -0,0 +1,8 @@
+//--- Navengando pelos elementos
+
+// parentNode parentElement
+const body = document.querySelector('body')
+    console.log(body.parentNode)
+
+const element = document.querySelector('h1')
+    console.log(body.parentElement)
